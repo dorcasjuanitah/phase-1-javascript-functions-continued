0b5feb7d89e6243e2adda2f44ca7aa8298727196
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-03-20 13:42:59 +0300

    adds functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(someString = 'roller-skate'){
+    return `This Saturday, I want to ${someString}!`;
+}
+
+function mondayWork(someString='go to the office'){
+    return `This Monday, I will ${someString}.`
+}
+
+function wrapAdjective(wrapper){
+return function (someString = 'special'){
+    return `You are ${wrapper}${someString}${wrapper}!`;
+}
+}
+console.log(wrapAdjective('||'));
